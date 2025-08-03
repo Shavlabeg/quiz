695a68f28d80c819a1f0e57a9e2c3e77d186cfa2
User <vladshavel09@gmail.com>
2025-08-03 11:45:48 +0300

    Added Feature file

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import time
+
+
 questions = {
     'Какой атомный номер  имеет водород': '1' ,
      'Сколько электронов в водороде': '1'
@@ -8,9 +11,18 @@ score = 0
 
 
 for q, a in questions.items():
+    start_time = time.time()
     user_answer = input(q + '')
+    end_time = time.time()
+    if round(end_time - start_time, 1) > 5 :
+        print('Вы не уложились в таймер 5 секунд')
+        user_answer = 'wrong'
+
+
+    print(f'Время ответа: {round(end_time - start_time, 1)} сек.')
     if user_answer.lower() == a.lower():
         print('Правильно')
+        score += 1
     else:
         print('Неверно')
 print(f'Ваш результат : {score} из {len(questions)}')
